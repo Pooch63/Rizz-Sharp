9a2ba66e88ba9ebf16eb62649a4e8a77f821d8f3
Pooch63 <99148663+Pooch63@users.noreply.github.com>
2025-04-03 19:29:17 -0400

    tons of files for literally two line changes

--- docs-src/src/components/HomepageFeatures/index.tsx
+++ docs-src/src/components/HomepageFeatures/index.tsx
@@ -40,11 +40,11 @@ export default function HomepageFeatures(): ReactNode {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
+        {/* <div className="row"> */}
+        {FeatureList.map((props, idx) => (
+          <Feature key={idx} {...props} />
+        ))}
+        {/* </div> */}
       </div>
     </section>
   );
