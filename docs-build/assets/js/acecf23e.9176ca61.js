f09979ca3dd07e9aa4ff31a0ec5e22cd3e6d0db0
Pooch63 <99148663+Pooch63@users.noreply.github.com>
2025-04-03 19:12:17 -0400

    pushed docs

--- docs-build/assets/js/acecf23e.9176ca61.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/Rizz-Sharp/docs/blog","blogTitle":"Blog","authorsListPath":"/Rizz-Sharp/docs/blog/authors"}')}}]);
---
+++ docs-src/build.bat
@@ -0,0 +1,3 @@
+cmd /c npm run build
+cd ../
+cmd /c mv /build/* ../docs/*
