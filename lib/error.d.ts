46c7f70fe231704b62aa9cf9074a588047f60f3c
Pooch63 <99148663+Pooch63@users.noreply.github.com>
2025-04-04 19:36:30 -0400

    Build process removes .d.ts

--- lib/error.d.ts
+++
@@ -1,7 +0,0 @@
-export type Position = {
-    line: number;
-    col: number;
-    length: number;
-};
-export declare function error(lines: string[], position: Position, message: string): string;
-export declare function warning(lines: string[], position: Position, message: string): string;
